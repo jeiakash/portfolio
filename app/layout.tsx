00b6dfc10b2c3869d9e4c8f03d4b48dbeb6ffdd3
jeiakash <168157789+jeiakash@users.noreply.github.com>
2025-07-10 10:51:17 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import "./globals.css"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Kelly - Personal Portfolio Template",
+  title: "JEI AKASH",
   description: "Free Bootstrap 5 portfolio template for personal website.",
     generator: 'v0.dev'
 }
